ed8ee483a48aa717dedd8fbafea72e210dc88d25
null <suneetk92@gmail.com>
2025-02-20 02:31:06 +0000

    chore(deps): update dependency moby/moby to v28

--- compile-docker.sh
+++ compile-docker.sh
@@ -8,7 +8,7 @@ echo "Docker compiler (client and server) for CoreELEC systems"
 BUILDX_VERSION="0.20.1"
 CTOP_VERSION="0.7.7"
 COMPOSE_VERSION="2.32.4"
-MOBY_VERSION="27.5.1"
+MOBY_VERSION="28.0.0"
 CLI_VERSION="27.5.1"
 
 # Prefix definitions
